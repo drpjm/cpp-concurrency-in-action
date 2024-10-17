b4fbfc3fa5bf8b36d7977ed71f89612adb172d21
Patrick Martin <patrickmartin@fastmail.com>
2024-10-17 11:15:19 -0400

    Added attribution and comments.

--- include/utils/threadsafe_queue.hpp
+++ include/utils/threadsafe_queue.hpp
@@ -6,27 +6,34 @@
 #include <condition_variable>
 #include <memory>
 
+/**
+ * This ThreadsafeQueue is from the C++ Concurrency in Action book
+ * with some small modifications.
+ */
+
 template<typename T>
 class ThreadsafeQueue
 {
 private:
     mutable std::mutex mut;
+    // stdlib queue - no reason to reinvent the wheel!
     std::queue<T> data_queue;
+    // Sets up signaling to ensure correct pushing/popping on the queue.
     std::condition_variable data_cond;
 public:
-    ThreadsafeQueue()
-    {}
-
+    
+    ThreadsafeQueue() {}
+    
     void push(T new_value)
     {
-        std::lock_guard<std::mutex> lk(mut);
+        std::lock_guard lk(mut);
         data_queue.push(std::move(new_value));
         data_cond.notify_one();
     }
 
     void wait_and_pop(T& value)
     {
-        std::unique_lock<std::mutex> lk(mut);
+        std::unique_lock lk(mut);
         data_cond.wait(lk,[this]{return !data_queue.empty();});
         value=std::move(data_queue.front());
         data_queue.pop();
@@ -34,7 +41,7 @@ public:
 
     std::shared_ptr<T> wait_and_pop()
     {
-        std::unique_lock<std::mutex> lk(mut);
+        std::unique_lock lk(mut);
         data_cond.wait(lk,[this]{return !data_queue.empty();});
         std::shared_ptr<T> res(
             std::make_shared<T>(std::move(data_queue.front())));
@@ -44,7 +51,7 @@ public:
 
     bool try_pop(T& value)
     {
-        std::lock_guard<std::mutex> lk(mut);
+        std::lock_guard lk(mut);
         if(data_queue.empty())
             return false;
         value=std::move(data_queue.front());
@@ -53,7 +60,7 @@ public:
 
     std::shared_ptr<T> try_pop()
     {
-        std::lock_guard<std::mutex> lk(mut);
+        std::lock_guard lk(mut);
         if(data_queue.empty())
             return std::shared_ptr<T>();
         std::shared_ptr<T> res(
@@ -64,7 +71,7 @@ public:
 
     bool empty() const
     {
-        std::lock_guard<std::mutex> lk(mut);
+        std::lock_guard lk(mut);
         return data_queue.empty();
     }
 };
