e29752b3839fc6d93cfcd9b2261a39c08627aa09
Patrick Martin <patrickmartin@fastmail.com>
2024-10-17 11:21:21 -0400

    Small tweaks to lock initializations.

--- include/utils/threadsafe_queue.hpp
+++ include/utils/threadsafe_queue.hpp
@@ -15,6 +15,7 @@ template<typename T>
 class ThreadsafeQueue
 {
 private:
+    // mutable keyword allows modification of the mutex value.
     mutable std::mutex mut;
     // stdlib queue - no reason to reinvent the wheel!
     std::queue<T> data_queue;
@@ -26,14 +27,14 @@ public:
     
     void push(T new_value)
     {
-        std::lock_guard lk(mut);
+        std::lock_guard lk { mut };
         data_queue.push(std::move(new_value));
         data_cond.notify_one();
     }
 
     void wait_and_pop(T& value)
     {
-        std::unique_lock lk(mut);
+        std::unique_lock lk { mut };
         data_cond.wait(lk,[this]{return !data_queue.empty();});
         value=std::move(data_queue.front());
         data_queue.pop();
@@ -41,7 +42,7 @@ public:
 
     std::shared_ptr<T> wait_and_pop()
     {
-        std::unique_lock lk(mut);
+        std::unique_lock lk { mut };
         data_cond.wait(lk,[this]{return !data_queue.empty();});
         std::shared_ptr<T> res(
             std::make_shared<T>(std::move(data_queue.front())));
@@ -51,7 +52,7 @@ public:
 
     bool try_pop(T& value)
     {
-        std::lock_guard lk(mut);
+        std::lock_guard lk { mut };
         if(data_queue.empty())
             return false;
         value=std::move(data_queue.front());
@@ -60,7 +61,7 @@ public:
 
     std::shared_ptr<T> try_pop()
     {
-        std::lock_guard lk(mut);
+        std::lock_guard lk { mut };
         if(data_queue.empty())
             return std::shared_ptr<T>();
         std::shared_ptr<T> res(
@@ -71,7 +72,7 @@ public:
 
     bool empty() const
     {
-        std::lock_guard lk(mut);
+        std::lock_guard lk { mut };
         return data_queue.empty();
     }
 };
